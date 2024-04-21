e897741ea20259c79364e2f3557309113fcdc60f
Trần Hữu Tài <99662952+Tran-Huu-Tai-12-04-23@users.noreply.github.com>
2024-04-21 22:44:52 +0700

    manager user , pagination filter data

--- obj/Debug/net8.0/course-edu-api.AssemblyInfo.cs
+++ obj/Debug/net8.0/course-edu-api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("course-edu-api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dc1359d5068dea35a8a85f91521b9a878603ad83")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+037158be046ce1e0abe2722e08f83fcf50e7701d")]
 [assembly: System.Reflection.AssemblyProductAttribute("course-edu-api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("course-edu-api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
